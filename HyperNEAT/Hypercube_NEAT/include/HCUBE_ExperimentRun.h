df01f01e1ec50b300b096719c7c35e293f1c09a3
Florian Golemo <fgolemo@gmail.com>
2014-10-09 02:07:43 +0200

    adjusted formatting, fixed some bugs in main and created function stubs for the 2 main functions.

--- HyperNEAT/Hypercube_NEAT/include/HCUBE_ExperimentRun.h
+++ HyperNEAT/Hypercube_NEAT/include/HCUBE_ExperimentRun.h
@@ -5,19 +5,18 @@
 
 #include "HCUBE_Defines.h"
 
-namespace HCUBE
-{
-    class ExperimentRun
-    {
+namespace HCUBE {
+
+    class ExperimentRun {
     public:
     protected:
         bool running;
         bool started;
         bool cleanup;
         int experimentType;
-				int currentSubExperiment;
-				int totalSubExperiments;
-						
+        int currentSubExperiment;
+        int totalSubExperiments;
+
         shared_ptr<NEAT::GeneticPopulation> population;
 
         vector<shared_ptr<Experiment> > experiments;
@@ -27,102 +26,89 @@ namespace HCUBE
         MainFrame *frame;
 
         string outputFileName;
-				
+
     public:
         ExperimentRun();
 
         virtual ~ExperimentRun();
 
-        inline void setCleanup(bool value)
-        {
+        inline void setCleanup(bool value) {
             cleanup = value;
         }
 
-        inline shared_ptr<NEAT::GeneticPopulation> getPopulation()
-        {
+        inline shared_ptr<NEAT::GeneticPopulation> getPopulation() {
             return population;
         }
 
-        inline shared_ptr<Experiment> getExperiment()
-        {
+        inline shared_ptr<Experiment> getExperiment() {
             return experiments[currentSubExperiment];
         }
 
-        inline void setFrame(MainFrame *_frame)
-        {
-            frame=_frame;
+        inline void setFrame(MainFrame *_frame) {
+            frame = _frame;
         }
 
-        inline const bool &isRunning()
-        {
+        inline const bool &isRunning() {
             return running;
         }
 
-        inline void setRunning(bool newRunning)
-        {
-            if (!population)
-            {
+        inline void setRunning(bool newRunning) {
+            if (!population) {
                 throw CREATE_LOCATEDEXCEPTION_INFO("Tried to start an experiment with no population!");
             }
 
             running = newRunning;
         }
 
-        inline const bool &isStarted()
-        {
+        inline const bool &isStarted() {
             return started;
         }
 
-        inline shared_ptr<NEAT::GeneticGeneration> getGeneration(int generation)
-        {
+        inline shared_ptr<NEAT::GeneticGeneration> getGeneration(int generation) {
             return population->getGeneration(generation);
         }
 
-        inline shared_ptr<NEAT::GeneticIndividual> getIndividual(int generation,int individual)
-        {
-            return population->getIndividual(individual,generation);
+        inline shared_ptr<NEAT::GeneticIndividual> getIndividual(int generation, int individual) {
+            return population->getIndividual(individual, generation);
         }
 
-        inline shared_ptr<NEAT::GeneticIndividual> getIndividual(int individual)
-        {
+        inline shared_ptr<NEAT::GeneticIndividual> getIndividual(int individual) {
             return population->getIndividual(individual);
         }
 
         void setupExperimentInProgress(
-            string populationFileName,
-            string _outputFileName
-        );
-		
-		void setupExperimentInProgressUseDat(
-															string _datFile, 
-															string populationFileName,
-															string _outputFileName
-															);
+                string populationFileName,
+                string _outputFileName
+                );
+
+        void setupExperimentInProgressUseDat(
+                string _datFile,
+                string populationFileName,
+                string _outputFileName
+                );
 
         void start();
 
         void setupExperiment(
-            int experimentType,
-            string _outputFileName
-        );
+                int experimentType,
+                string _outputFileName
+                );
 
-        void createPopulation(string populationString="");
+        void createPopulation(string populationString = "");
 
         virtual void evaluatePopulation();
 
         void finishEvaluations();
 
-        void produceNextGeneration(); 
+        void produceNextGeneration();
         void experimentRunPrintToGenChampFile();
 
-
-        inline mutex* getPopulationMutex()
-        {
+        inline mutex* getPopulationMutex() {
             return populationMutex;
         }
 
     protected:
-			bool switchSubExperiment(int);
+        bool switchSubExperiment(int);
 
     };
 }
